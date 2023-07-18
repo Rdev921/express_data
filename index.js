c46182d5971dc02c5d84e53827f1901b20229982
rishabh dev <rishabhd921@gmail.com>
2023-07-18 14:45:43 +0530

    express_data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();  
-const router = jsonServer.router(data); 
+const router = jsonServer.router(db.json); 
 const middlewares = jsonServer.defaults({ logger: true });
 const port = process.env.PORT || 8080;
 
